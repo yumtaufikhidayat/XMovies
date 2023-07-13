f119d2533d5b6ccbe8ab4438b2ac0af8cb8b6b11
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-14 05:11:56 +0700

    Add Constants.kt class.

---
+++ app/src/main/java/com/yumtaufikhidayat/xnews/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.yumtaufikhidayat.xnews.utils
+
+object Constants {
+    const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+    const val STARTING_PAGE_INDEX = 1
+    const val LIMIT_PER_INDEX = 50
+    const val COUNTRY = "us"
+}
