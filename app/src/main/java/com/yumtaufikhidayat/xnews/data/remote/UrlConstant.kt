8d146a9d9a4a754413c84cc68f6d8fba0a8f9944
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-14 05:16:38 +0700

    Add UrlConstant.

---
+++ app/src/main/java/com/yumtaufikhidayat/xnews/data/remote/UrlConstant.kt
@@ -0,0 +1,10 @@
+package com.yumtaufikhidayat.xnews.data.remote
+
+object UrlConstant {
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val ENDPOINT_TOP_HEADLINES = "top-headlines"
+    const val QUERY_COUNTRY = "country"
+    const val QUERY_PAGE_SIZE = "pageSize"
+    const val QUERY_PAGE = "page"
+    const val QUERY_API_KEY = "apiKey"
+}
