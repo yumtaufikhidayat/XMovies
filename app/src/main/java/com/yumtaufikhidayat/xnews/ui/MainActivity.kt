8f81d09a90dfd739f4887bbe0566d80e4b91c453
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-14 05:14:40 +0700

    Add @AndroidEntryPoint annotation.

--- app/src/main/java/com/yumtaufikhidayat/xnews/ui/MainActivity.kt
+++ app/src/main/java/com/yumtaufikhidayat/xnews/ui/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.yumtaufikhidayat.xnews
+package com.yumtaufikhidayat.xnews.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import com.yumtaufikhidayat.xnews.R
 import com.yumtaufikhidayat.xnews.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
