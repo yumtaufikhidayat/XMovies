6590f6bdb10cd2f52b0f095c7fbd42d92180adc2
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-13 10:56:12 +0700

    Add SplashScreenFragment, HomeFragment, and DetailFragment classes.

---
+++ app/src/main/java/com/yumtaufikhidayat/xnews/ui/detail/DetailFragment.kt
@@ -0,0 +1,37 @@
+package com.yumtaufikhidayat.xnews.ui.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yumtaufikhidayat.xnews.databinding.FragmentDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    companion object {
+        const val EXTRA_DATA = "com.yumtaufikhidayat.xnews.ui.detail.EXTRA_DATA"
+    }
+}
---
+++ app/src/main/java/com/yumtaufikhidayat/xnews/ui/splashscreen/SplashScreenFragment.kt
@@ -0,0 +1,33 @@
+package com.yumtaufikhidayat.xnews.ui.splashscreen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yumtaufikhidayat.xnews.databinding.FragmentSplashScreenBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashScreenFragment : Fragment() {
+
+    private var _binding: FragmentSplashScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
