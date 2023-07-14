f53a0c35b2381d4846900b5443259ce403f0b5b8
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-14 09:36:04 +0700

    Implement news detail in DetailFragment.kt class.

--- app/src/main/java/com/yumtaufikhidayat/xnews/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/yumtaufikhidayat/xnews/ui/detail/DetailFragment.kt
@@ -1,10 +1,15 @@
 package com.yumtaufikhidayat.xnews.ui.detail
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.yumtaufikhidayat.xnews.R
 import com.yumtaufikhidayat.xnews.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +18,8 @@ class DetailFragment : Fragment() {
 
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
+
+    private var newsUrl: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +31,64 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initToolbarDetail()
+        getBundleData()
+        showNewsWebView()
+    }
+
+    private fun initToolbarDetail() {
+        binding.toolbarDetailNews.apply {
+            tvToolbarTitle.text = getString(R.string.tvNewsDetail)
+            imgBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    private fun getBundleData() {
+        newsUrl = arguments?.getString(EXTRA_DATA).orEmpty()
+    }
+
+    private fun showNewsWebView() {
+        binding.apply {
+            webViewDetailNews.apply {
+                settings.loadsImagesAutomatically = true
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                settings.supportZoom()
+                settings.builtInZoomControls = true
+                settings.displayZoomControls = true
+                scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
+                webViewClient = WebViewClient()
+                loadUrl(newsUrl)
+                shareNews(newsUrl)
+            }
+        }
+    }
+
+    private fun shareNews(newsUrl: String) {
+        binding.toolbarDetailNews.imgShare.setOnClickListener {
+            try {
+                val body = getString(R.string.tvShareNews, newsUrl)
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                shareIntent.apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, body)
+                }
+                startActivity(
+                    Intent.createChooser(
+                        shareIntent,
+                        getString(R.string.tvShareWith)
+                    )
+                )
+            } catch (e: Exception) {
+                Toast.makeText(
+                    requireContext(),
+                    StringBuilder(getString(R.string.tvOops)).append("\n$e"), Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 
     override fun onDestroyView() {
